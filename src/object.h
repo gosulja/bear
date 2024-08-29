d2d43e5447eee470b982fe835ad8a8be84ee8624
blinx <111649405+gosulja@users.noreply.github.com>
2024-08-29 14:33:10 +0100

    fixed shit

--- src/object.h
+++ src/object.h
@@ -16,14 +16,15 @@ typedef enum {
 } ObjType;
 
 struct Obj {
-  ObjType type;
+  ObjType     type;
   struct Obj* next;
 };
 
 struct ObjString {
-  Obj obj;
-  int length;
-  char* chars;
+  Obj       obj;
+  int       length;
+  char*     chars;
+  uint32_t  hash;
 };
 
 ObjString* takeString(char* chars, int length);
--- src/value.c
+++ src/value.c
@@ -35,6 +35,7 @@ void printValue(Value value) {
       break;
     case VAL_NIL: printf("nil"); break;
     case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    case VAL_OBJ: printObject(value); break;
   }
 }
 
@@ -45,11 +46,11 @@ bool valuesEqual(Value a, Value b) {
     case VAL_NIL:    return true;
     case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
     case VAL_OBJ: {
-      ObjString* aString = AS_STRING(a);
-      ObjString* bString = AS_STRING(b);
-      return aString->length == bString->length &&
-          memcmp(aString->chars, bString->chars,
-                 aString->length) == 0;
+      ObjString* aStr = AS_STRING(a);
+      ObjString* bStr = AS_STRING(b);
+      return aStr->length == bStr->length &&
+        memcmp(aStr->chars, bStr->chars,
+               aStr->length) == 0;
     }
     default:         return false; // Unreachable.
   }
